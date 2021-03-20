338d7e19d59a77397e98099c488734aa7ff982d5
faisalmalik47 <malikf951@gmail.com>
2021-03-20 15:28:55 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,222 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {
+    "id": "G9Y94Kgo3kBn"
+   },
+   "source": [
+    "# 1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.\n",
+    "\n",
+    "*\n",
+    "\n",
+    "&#39;hello&#39;\n",
+    "\n",
+    "-87.8\n",
+    "\n",
+    "-\n",
+    "\n",
+    "/\n",
+    "\n",
+    "+\n",
+    "\n",
+    "6"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {
+    "id": "5Poe5HZb4OPX"
+   },
+   "source": [
+    "Ans: \n",
+    "\n",
+    "\"hello\", -87.8, 6 are the values while\n",
+    "\n",
+    "*, -, /, + are expressions\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {
+    "id": "cZeB8ScG4j3M"
+   },
+   "source": [
+    "# 2. What is the difference between string and variable?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {
+    "id": "Sh9XvyXJ40Qw"
+   },
+   "source": [
+    "Ans:\n",
+    "\n",
+    "A variable stores an information like values of int floats etc, while a string stores an a value representing a text of an array of information"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 3. Describe three different data types."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "A data type is a classification of data, which devides the data on the baisi of the natture of it, for example int, float, string, char, bool etc"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 4. What is an expression made up of? What do all expressions do?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "An expression is made of values and operators, functions and etc that needs to be evaluated, expressions generally equationizes a problem statement"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "An expression only contains variables, functions, operators and all, like in the question above \"spam = 10\" is a vvariable while a statement is everything that can make up a python code."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 6. After running the following code, what does the variable bacon contain?\n",
+    "# bacon = 22\n",
+    "# bacon + 1"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "Ans: \n",
+    "bacon contains a value of 22 because it is incremented"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 7. What should the values of the following two terms be?\n",
+    "# &#39;spam&#39; + &#39;spamspam&#39;\n",
+    "# &#39;spam&#39; * 3"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "both the terms would give the same output that is spamspamspam "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 8. Why is eggs a valid variable name while 100 is invalid?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "Egg is a valid variable name because it is unique and not a value while 100 is a value so it cannot be used as a variable name"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "int(input())\n",
+    "\n",
+    "float(input())\n",
+    "\n",
+    "str(input())"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# 10. Why does this expression cause an error? How can you fix it?\n",
+    "\n",
+    "# &#39;I have eaten &#39; + 99 + &#39; burritos.&#39;"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "We cannot concatenate different types of data types."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "colab": {
+   "authorship_tag": "ABX9TyOOPE0jsSneswEHoH8p26lX",
+   "collapsed_sections": [],
+   "name": "Untitled1.ipynb",
+   "provenance": []
+  },
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 1
+}
